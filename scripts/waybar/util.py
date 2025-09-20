cd63ba32ba538b8a3b6f53407ffc1ede85d83b42
gdanko <gdanko@protonmail.com>
2025-09-19 21:06:38 -0700

    Remove is_worker_running; add interface_exists and interface_is_connected

--- scripts/waybar/util.py
+++ scripts/waybar/util.py
@@ -83,31 +83,6 @@ def process_is_running(name: str=None, full: bool=False):
     else:
         return False, []
 
-def is_worker_running(lockfile: Path) -> bool:
-    """
-    Determine if a worker for a given module is running
-    """
-
-    if not lockfile.exists():
-        return False
-    try:
-        pid = int(lockfile.read_text())
-        proc = psutil.Process(pid)
-        cmdline = proc.cmdline()
-        # Only consider it running if it's our script with 'worker' arg
-        if __file__ in cmdline[0] and 'worker' in cmdline:
-            return True
-        else:
-            lockfile.unlink()
-            return False
-    except (ValueError, FileNotFoundError, psutil.NoSuchProcess, PermissionError):
-        # Stale lockfile
-        try:
-            lockfile.unlink()
-        except Exception:
-            pass
-        return False
-
 #==========================================================
 #  Unit conversersion
 #==========================================================
@@ -309,6 +284,15 @@ def validate_requirements(required: list=[]):
             icon    = icon,
             message = f'Please install via pip: {", ".join(missing)}',
         )
+
+def interface_exists(interface: str=None) -> bool:
+    return os.path.isdir(f'/sys/class/net/{interface}')
+    # cat /sys/class/net/enp1s0/carrier
+
+def interface_is_connected(interface: str=None) -> bool:
+    with open(f'/sys/class/net/{interface}/carrier', 'r') as f:
+        contents = f.read()
+    return True if int(contents) == 1 else False
 
 #==========================================================
 #  Formatting and conversion

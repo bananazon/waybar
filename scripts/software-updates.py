0a6a4679af84616a78826aa5ce301882e717113b
gdanko <gdanko@protonmail.com>
2025-09-26 00:18:38 -0700

    Start to add tooltips

--- scripts/software-updates.py
+++ scripts/software-updates.py
@@ -23,11 +23,17 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 LOGFILE = CACHE_DIR / 'waybar-software-updates.log'
 VALID_TYPES = ['apt', 'brew', 'dnf', 'flatpak', 'mintupdate', 'pacman', 'snap', 'yay', 'yay-aur', 'yum']
 
+class Package(NamedTuple):
+    available_version : Optional[str]  = None
+    installed_version : Optional[str]  = None
+    name              : Optional[str]  = None
+
 class SystemUpdates(NamedTuple):
-    success  : Optional[bool] = False
-    error    : Optional[str]  = None
-    count    : Optional[int]  = 0
-    packages : Optional[List[str]] = None
+    success      : Optional[bool] = False
+    error        : Optional[str]  = None
+    package_type : Optional[str]  = None
+    count        : Optional[int]  = 0
+    packages     : Optional[List[str]] = None
 
 logging.basicConfig(
     filename=LOGFILE,
@@ -35,6 +41,30 @@ logging.basicConfig(
     format='%(asctime)s [%(levelname)-5s] - %(message)s',
     level=logging.INFO
 )
+
+def generate_tooltip(data):
+    tooltip = [
+    ]
+    count = len(data.packages)
+    max_shown = 20
+    max_name_len = 0
+    max_version_len = 0
+
+    for item in data.packages:
+        package_name = item.name.split('.')[0]
+        if len(item.name.split('.')[0]) > max_name_len:
+            max_name_len = len(item.name.split('.')[0])
+        
+        if len(item.installed_version) > max_version_len:
+            max_version_len = len(item.installed_version)
+
+    for item in data.packages[:max_shown]:
+        tooltip.append(f'{item.name.split('.')[0][:max_name_len]:{max_name_len}} {item.installed_version:{max_version_len}} => {item.available_version}')
+    
+    if count > max_shown:
+        tooltip.append(f'and {count - max_shown} more...')
+
+    return '\n'.join(tooltip)
 
 def find_apt_updates(package_type: str = None):
     """
@@ -107,13 +137,21 @@ def find_brew_updates(package_type: str = None):
 
     packages = []
     for obj in brew_data['formulae']:
-        packages.append(obj['name'])
+        packages.append(Package(
+            name              = obj.get('name'),
+            available_version = obj.get('current_version'),
+            installed_version = obj.get('installed_versions')[0],
+        ))
 
     for obj in brew_data['casks']:
-        packages.append(obj['name'])
+        packages.append(Package(
+            name              = obj.get('name'),
+            available_version = obj.get('current_version'),
+            installed_version = obj.get('installed_versions')[0],
+        ))
 
     logging.info(f'[find_brew_updates] returning data, package_type={package_type}')
-    return SystemUpdates(success=True, count=len(packages), packages=packages)
+    return SystemUpdates(success=True, count=len(packages), packages=packages, package_type=package_type)
 
 def find_dnf_updates(package_type: str=None):
     """
@@ -121,30 +159,50 @@ def find_dnf_updates(package_type: str=None):
     """
     logging.info(f'[find_dnf_updates] entering function, type={package_type}')
 
-    command = f'sudo dnf update -y'
-    rc, stdout, stderr = util.run_piped_command(command)
-    if rc != 0:
-        return SystemUpdates(success=False, error=f'Failed to execute "{command}"')
+    packages = []
+    available_map = {}
+    installed_map = {}
 
-    command = f'sudo dnf check-update'
+    command = f'sudo dnf check-upgrade'
     rc, stdout, stderr = util.run_piped_command(command)
     if rc == 0:
-        packages = []
-        try:
-            _, after = stdout.split('Repositories loaded.', 1)
-            lines = after.lstrip().strip().split('\n')
-        except:
-            logging.info(f'[find_dnf_updates] regex not found, please run "{command}" manually to verify')
-            lines = []
-
-        for line in lines:
+        return SystemUpdates(success=True, packages=[], package_type=package_type)
+    elif rc == 1:
+        return SystemUpdates(success=False, error=f'Failed to execute "{command}"', package_type=package_type)
+    elif rc == 100:
+        for line in stdout.split('\n'):
             bits = re.split(r'\s+', line)
-            packages.append(bits[0])
-    else:
-        return SystemUpdates(success=False, error=f'Failed to execute "{command}"')
+            if len(bits) == 3:
+                available_map[bits[0]] = {
+                    'available_version': bits[1]
+                }
 
-    logging.info(f'[find_dnf_updates] returning data, package_type={package_type}')
-    return SystemUpdates(success=True, count=len(packages), packages=packages)
+    command = f'sudo dnf list --installed'
+    rc, stdout, stderr = util.run_piped_command(command)
+    if rc == 0:
+        match = re.search(r"Installed packages\n([\s\S]*)", stdout)
+        if match:
+            for line in match.group(1).split('\n'):
+                bits = re.split(r'\s+', line)
+                if len(bits) == 3:
+                    installed_map[bits[0]] = {
+                        'installed_version': bits[1]
+                    }
+    else:
+        return SystemUpdates(success=False, error=f'Failed to execute "{command}"', package_type=package_type)
+    
+    for key, value in available_map.items():
+        if key in installed_map:
+            available_map[key]['installed_version'] = installed_map[key]['installed_version']
+
+    for key, value in available_map.items():
+        packages.append(Package(
+            name              = key,
+            available_version = value.get('available_version'),
+            installed_version = value.get('installed_version'),
+        ))
+
+    return SystemUpdates(success=True, count=len(packages), packages=packages, package_type=package_type)
 
 def find_flatpak_updates(package_type: str=None):
     """
@@ -356,7 +414,7 @@ def find_updates(package_type: str = ''):
 
     return data
 
-def worker(type: str=None):
+def worker(package_type: str=None):
     while True:
         update_event.wait()
         update_event.clear()
@@ -367,36 +425,37 @@ def worker(type: str=None):
             sys.exit(0)
         else:
             if util.network_is_reachable():
-                logging.info(f'[run] Starting - package_type={type}')
+                logging.info(f'[run] Starting - package_type={package_type}')
 
 
                 loading_dict = {
-                    'text'    : f'{glyphs.md_timer_outline}{glyphs.icon_spacer}Checking {type}...',
+                    'text'    : f'{glyphs.md_timer_outline}{glyphs.icon_spacer}Checking {package_type}...',
                     'class'   : 'loading',
-                    'tooltip' : f'Checking {type}',
+                    'tooltip' : f'Checking {package_type}',
                 }
                 print(json.dumps(loading_dict))
 
-                data = find_updates(package_type=type)
+                data = find_updates(package_type=package_type)
 
                 if data.success:
+                    tooltip = generate_tooltip(data)
                     packages = 'package' if data.count == 1 else 'packages'
                     output = {
-                        'text'    : f'{glyphs.md_package_variant}{glyphs.icon_spacer}{type} {data.count} outdated {packages}',
+                        'text'    : f'{glyphs.md_package_variant}{glyphs.icon_spacer}{package_type} {data.count} outdated {packages}',
                         'class'   : 'success',
-                        'tooltip' : f'{type} updates',
+                        'tooltip' : tooltip,
                     }
                 else:
                     output = {
-                        'text'    : f'{glyphs.md_package_variant}{glyphs.icon_spacer}{type} failed to find updates',
+                        'text'    : f'{glyphs.md_package_variant}{glyphs.icon_spacer}{package_type} failed to find updates',
                         'class'   : 'success',
-                        'tooltip' : f'{type} updates',
+                        'tooltip' : f'{package_type} update error',
                     }
             else:
                 output= {
                     'text'    : f'{glyphs.md_alert}{glyphs.icon_spacer}the network is unreachable',
                     'class'   : 'error',
-                    'tooltip' : f'{apt} error',
+                    'tooltip' : f'{package_type} update error',
                 }
 
             print(json.dumps(output))
@@ -408,12 +467,11 @@ def refresh_handler(signum, frame):
 signal.signal(signal.SIGHUP, refresh_handler)
 
 @click.command(help='Check available system updates from different sources', context_settings=CONTEXT_SETTINGS)
-@click.option('-t', '--type', required=True, help=f'The type of update to query; valid choices are: {", ".join(VALID_TYPES)}')
+@click.option('-t', '--package-type', required=True, help=f'The type of update to query; valid choices are: {", ".join(VALID_TYPES)}')
 @click.option('-i', '--interval', type=int, default=1800, help='The update interval (in seconds)')
-def main(type, interval):
+def main(package_type, interval):
     logging.info('[main] entering')
-
-    threading.Thread(target=worker, args=(type,), daemon=True).start()
+    threading.Thread(target=worker, args=(package_type,), daemon=True).start()
     update_event.set()
 
     while True:

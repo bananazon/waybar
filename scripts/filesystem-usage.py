05fbc7bdc7c6bddd305d5be7af5cfc6abdef449a
gdanko <gdanko@protonmail.com>
2025-10-08 12:08:39 -0700

    filesystem-usage now handles multiple mountpoints

--- scripts/filesystem-usage.py
+++ scripts/filesystem-usage.py
@@ -22,14 +22,20 @@ condition        = threading.Condition()
 context_settings = dict(help_option_names=['-h', '--help'])
 disk_info        = None
 format_index     = 0
-formats          = [0, 1, 2]
-loading          = f'{glyphs.md_timer_outline}{glyphs.icon_spacer}Working...'
-loading_dict     = { 'text': loading, 'class': 'loading', 'tooltip': 'Gathering disk statistics'}
-mountpoint_name  = None
+logfile          = cache_dir / f'waybar-filesystem-usage.log'
 needs_fetch      = False
 needs_redraw     = False
 
 sys.stdout.reconfigure(line_buffering=True)
+
+class DiskStatsEntry(NamedTuple):
+    first_sample  : Optional[namedtuple] = None
+    second_sample : Optional[namedtuple] = None
+ 
+class DiskStats(NamedTuple):
+    success    : Optional[bool] = False
+    error      : Optional[str]  = None
+    entries    : Optional[list[DiskStatsEntry]] = None
 
 class FilesystemInfo(NamedTuple):
     success    : Optional[bool] = False
@@ -48,8 +54,9 @@ class FilesystemInfo(NamedTuple):
     used       : Optional[int]  = 0
     sample1    : Optional[namedtuple] = None
     sample2    : Optional[namedtuple] = None
+    updated    : Optional[str]  = None
 
-def configure_logging(debug: bool=False, logfile: str=None):
+def configure_logging(debug: bool=False):
     logging.basicConfig(
         filename = logfile,
         filemode = 'w',  # 'a' = append, 'w' = overwrite
@@ -59,7 +66,7 @@ def configure_logging(debug: bool=False, logfile: str=None):
 
 def refresh_handler(signum, frame):
     global needs_fetch, needs_redraw
-    logging.info(f'[refresh_handler] - received SIGHUP — re-fetching data for mountpoint {mountpoint_name}')
+    logging.info(f'[refresh_handler] - received SIGHUP — re-fetching data')
     with condition:
         needs_fetch = True
         needs_redraw = True
@@ -68,7 +75,11 @@ def refresh_handler(signum, frame):
 def toggle_format(signum, frame):
     global format_index, needs_redraw
     format_index = (format_index + 1) % len(formats)
-    logging.info(f'[toggle_format] - received SIGUSR1 - switching output format to {format_index + 1} for mountpoint {mountpoint_name}')
+    if disk_info and type(disk_info) == list:
+        mountpoint = disk_info[format_index].mountpoint
+    else:
+        mountpoint = format_index + 1
+    logging.info(f'[toggle_format] - received SIGUSR1 - switching output format to {mountpoint}')
     with condition:
         needs_redraw = True
         condition.notify()
@@ -114,7 +125,7 @@ def generate_tooltip(disk_info: namedtuple=None, show_stats: bool=False):
 
     if len(tooltip) > 0:
         tooltip.append('')
-        tooltip.append(f'Last updated {util.get_human_timestamp()}')
+        tooltip.append(f'Last updated {disk_info.updated}')
 
     return '\n'.join(tooltip)
 
@@ -123,36 +134,49 @@ def filesystem_exists(mountpoint: str = None):
     rc, _, _ = util.run_piped_command(command)
     return True if rc == 0 else False
 
-def get_sample(filesystem: str=None):
-    logging.debug(f'[get_sample] - entering with filesystem={filesystem}')
+def get_sample():
+    logging.debug(f'[get_sample] - entering function')
     command = f'cat /proc/diskstats| jc --pretty --proc-diskstats'
     rc, stdout, stderr = util.run_piped_command(command)
     if rc == 0 and stdout != '':
         json_data, err = util.parse_json_string(stdout)
         if not err:
+            entries = []
             for entry in json_data:
-                if entry['device'] == filesystem:
-                    sample = util.dict_to_namedtuple(name='DiskStatsSample', obj=entry)
-                    return sample
+                sample = util.dict_to_namedtuple(name='DiskStatsSample', obj=entry)
+                entries.append(sample)
+            return entries
 
     return None
 
-def get_disk_stats(filesystem: str=None):
-    logging.debug(f'[get_disk_stats] - entering with filesystem={filesystem}')
-    first = get_sample(filesystem=filesystem)
+def get_disk_stats():
+    logging.debug(f'[get_disk_stats] - entering function')
+    first_sample = get_sample()
     time.sleep(1)
-    second = get_sample(filesystem=filesystem)
+    second_sample = get_sample()
 
-    if not first or not second:
+    if not first_sample or not second_sample:
         return DiskStats(
             success = False,
             error   = 'failed to get disk stats'
         )
 
+    if len(first_sample) != len(second_sample):
+         return DiskStats(
+            success = False,
+            error   = 'failed to get disk stats'
+        )
+
+    entries = []
+    for idx, _ in enumerate(first_sample):
+        entries.append(DiskStatsEntry(
+            first_sample  = first_sample,
+            second_sample = second_sample,
+        ))
+
     return DiskStats(
         success        = True,
-        reads_per_sec  = (second.reads_completed - first.reads_completed),
-        writes_per_sec = (second.writes_completed - first.writes_completed),
+        entries        = entries,
     )
 
 def parse_lsblk(filesystem: str=None):
@@ -168,90 +192,99 @@ def parse_lsblk(filesystem: str=None):
 
     return None
 
-def get_disk_usage(mountpoint: str, show_stats: bool=False) -> list:
-    """
-    Execute df -B 1 against a mount point and return a namedtuple with its values
-    """
-    logging.debug(f'[get_disk_usage] - entering with mountpoint={mountpoint}')
-    df_item = None
-    findmnt_item = None
-    first = None
-    second = None
+def get_disk_usage(mountpoints: list=None, show_stats: bool=False):
+    logging.debug(f'[get_disk_usage] - entering with mountpoints={mountpoints}')
+    disk_usage = []
 
-    command = f'jc --pretty df {mountpoint}'
-    try:
-        rc, stdout, stderr = util.run_piped_command(command)
-        if rc == 0 and stdout != '':
-            df_data, err = util.parse_json_string(stdout)
-            if len(df_data) == 1:
-                df_item = df_data[0]
-        else:
-            return FilesystemInfo(success = False, error = stderr or f'failed to execute {command}')
-    except:
-        return FilesystemInfo(success = False, error = stderr or f'failed to execute {command}')
+    if show_stats:
+        first_sample = get_sample()
+        time.sleep(1)
+        second_sample = get_sample()
 
-    if df_item:
-        command = f'jc --pretty findmnt {mountpoint}'
+    for mountpoint in mountpoints:
+        df_item = None
+        findmnt_item = None
+        first = None
+        second = None
+
+        command = f'jc --pretty df {mountpoint}'
         try:
             rc, stdout, stderr = util.run_piped_command(command)
             if rc == 0 and stdout != '':
-                findmnt_data, err = util.parse_json_string(stdout)
-                if len(findmnt_data) == 1:
-                    findmnt_item = findmnt_data[0]
+                df_data, err = util.parse_json_string(stdout)
+                if len(df_data) == 1:
+                    df_item = df_data[0]
             else:
                 return FilesystemInfo(success = False, error = stderr or f'failed to execute {command}')
         except:
-            return FilesystemInfo(success = False, error   = stderr or f'failed to execute {command}')
+            return FilesystemInfo(success = False, error = stderr or f'failed to execute {command}')
+        
+        if df_item:
+            command = f'jc --pretty findmnt {mountpoint}'
+            try:
+                rc, stdout, stderr = util.run_piped_command(command)
+                if rc == 0 and stdout != '':
+                    findmnt_data, err = util.parse_json_string(stdout)
+                    if len(findmnt_data) == 1:
+                        findmnt_item = findmnt_data[0]
+                else:
+                    return FilesystemInfo(success = False, error = stderr or f'failed to execute {command}')
+            except:
+                return FilesystemInfo(success = False, error   = stderr or f'failed to execute {command}')
 
-        lsblk_data = parse_lsblk(filesystem=df_item['filesystem'])
-        if lsblk_data and show_stats:
-            first = get_sample(filesystem=lsblk_data.kname)
-            time.sleep(1)
-            second = get_sample(filesystem=lsblk_data.kname)
+            lsblk_data = parse_lsblk(filesystem=df_item['filesystem'])
+            if lsblk_data and show_stats:
+                first = [entry for entry in first_sample if entry.device == lsblk_data.kname][0]
+                second = [entry for entry in second_sample if entry.device == lsblk_data.kname][0]
 
-    if df_item and findmnt_item:
-        return FilesystemInfo(
-            success    = True,
-            filesystem = df_item['filesystem'],
-            mountpoint = mountpoint,
-            total      = df_item['1k_blocks'] * 1024,
-            used       = df_item['used'] * 1024,
-            free       = df_item['available'] * 1024,
-            pct_total  = 100,
-            pct_used   = df_item['use_percent'],
-            pct_free   = 100 - df_item['use_percent'],
-            fsopts     = findmnt_item.get('options') or None,
-            fstype     = findmnt_item.get('fstype') or None,
-            lsblk      = lsblk_data,
-            sample1    = first,
-            sample2    = second,
-        )
+        if df_item and findmnt_item:
+            disk_usage.append(FilesystemInfo(
+                success    = True,
+                filesystem = df_item['filesystem'],
+                mountpoint = mountpoint,
+                total      = df_item['1k_blocks'] * 1024,
+                used       = df_item['used'] * 1024,
+                free       = df_item['available'] * 1024,
+                pct_total  = 100,
+                pct_used   = df_item['use_percent'],
+                pct_free   = 100 - df_item['use_percent'],
+                fsopts     = findmnt_item.get('options') or None,
+                fstype     = findmnt_item.get('fstype') or None,
+                lsblk      = lsblk_data,
+                sample1    = first or None,
+                sample2    = second or None,
+                updated    = util.get_human_timestamp(),
+            ))
 
-def render_output(disk_info: namedtuple=None, unit: str=None, icon: str=None):
-    pct_total = disk_info.pct_total
-    pct_used  = disk_info.pct_used
-    pct_free  = disk_info.pct_free
-    total     = util.byte_converter(number=disk_info.total, unit=unit)
-    used      = util.byte_converter(number=disk_info.used, unit=unit)
-    free      = util.byte_converter(number=disk_info.free, unit=unit)
+    return disk_usage
 
-    if pct_free < 20:
-        output_class = 'critical'
-    elif pct_free < 50:
-        output_class = 'warning'
-    else:
-        output_class = 'good'
+def render_output(disk_info: namedtuple=None, unit: str=None, icon: str=None, show_stats: bool=False):
+    if disk_info.success:
+        pct_total = disk_info.pct_total
+        pct_used  = disk_info.pct_used
+        pct_free  = disk_info.pct_free
+        total     = util.byte_converter(number=disk_info.total, unit=unit)
+        used      = util.byte_converter(number=disk_info.used, unit=unit)
+        free      = util.byte_converter(number=disk_info.free, unit=unit)
 
-    if format_index == 0:
+        if pct_free < 20:
+            output_class = 'critical'
+        elif pct_free < 50:
+            output_class = 'warning'
+        else:
+            output_class = 'good'
+
         text = f'{icon}{glyphs.icon_spacer}{disk_info.mountpoint} {used} / {total}'
-    elif format_index == 1:
-        text = f'{icon}{glyphs.icon_spacer}{disk_info.mountpoint} {pct_used}% used'
-    elif format_index == 2:
-        text = f'{icon}{glyphs.icon_spacer}{disk_info.mountpoint} {used} used / {free} free'
+        output_class = output_class
+        tooltip = generate_tooltip(disk_info=disk_info, show_stats=show_stats)
+    else:
+        test = f'{glyphs.md_alert}{glyphs.icon_spacer}{disk_info.mountpoint} {disk_info.error}'
+        output_class = 'error'
+        tooltip = f'{disk_info.mountpoint} error'
 
-    return text, output_class
+    return text, output_class, tooltip
 
-def worker(mountpoint: str=None, unit: str=None, show_stats: bool=False):
+def worker(mountpoints: list=None, unit: str=None, show_stats: bool=False):
     global disk_info, needs_fetch, needs_redraw, format_index
 
     while True:
@@ -264,69 +297,60 @@ def worker(mountpoint: str=None, unit: str=None, show_stats: bool=False):
             needs_fetch  = False
             needs_redraw = False
 
-        if not filesystem_exists(mountpoint=mountpoint):
-            output = {
-                'text'    : f'{glyphs.md_harddisk}{glyphs.icon_spacer}{mountpoint} doesn\'t exist',
-                'class'   : 'error',
-                'tooltip' : 'Filesystem error',
-            }
-            print(json.dumps(output))
-            disk_info = None
-            continue    
+        # if not filesystem_exists(mountpoint=mountpoint):
+        #     output = {
+        #         'text'    : f'{glyphs.md_harddisk}{glyphs.icon_spacer}{mountpoint} doesn\'t exist',
+        #         'class'   : 'error',
+        #         'tooltip' : 'Filesystem error',
+        #     }
+        #     print(json.dumps(output))
+        #     disk_info = None
+        #     continue    
 
         if fetch:
-            if disk_info:
-                if disk_info.success:
-                    text, _ = render_output(disk_info=disk_info, unit=unit, icon=glyphs.md_timer_outline)
-                    print(json.dumps({'text': text, 'class': 'loading'}))
-                else:
-                    print(json.dumps(loading_dict))
+            loading      = f'{glyphs.md_timer_outline}{glyphs.icon_spacer}Working...'
+            loading_dict = { 'text': loading, 'class': 'loading', 'tooltip': 'Gathering disk statistics'}
+            if disk_info and type(disk_info) == list:
+                text, _, tooltip = render_output(disk_info=disk_info[format_index], unit=unit, icon=glyphs.md_timer_outline)
+                print(json.dumps({'text': text, 'class': 'loading', 'tooltip': tooltip}))
             else:
                 print(json.dumps(loading_dict))
-            disk_info = get_disk_usage(mountpoint=mountpoint, show_stats=show_stats)
+
+            disk_info = get_disk_usage(mountpoints=mountpoints, show_stats=show_stats)
 
         if disk_info is None:
             continue
 
-        if disk_info.success:
+        if disk_info and type(disk_info) == list:
             if redraw:
-                logging.debug(f'[worker] - successfully retrieved data for mountpoint {mountpoint}')
-                text, output_class = render_output(disk_info=disk_info, unit=unit, icon=glyphs.md_harddisk)
+                text, output_class, tooltip = render_output(disk_info=disk_info[format_index], unit=unit, icon=glyphs.md_harddisk, show_stats=show_stats)
 
                 output = {
                     'text'    : text,
                     'class'   : output_class,
-                    'tooltip' : generate_tooltip(disk_info=disk_info, show_stats=show_stats),
+                    'tooltip' : tooltip,
                 }
-        else:
-            output = {
-                'text'    : f'{glyphs.md_harddisk}{glyphs.icon_spacer}{mountpoint} {disk_info.error}',
-                'class'   : 'error',
-                'tooltip' : generate_tooltip(disk_info=disk_info, show_stats=show_stats),
-            }
 
-        print(json.dumps(output))
+                print(json.dumps(output))
 
 @click.command(help='Get disk informatiopn from df(1)', context_settings=context_settings)
-@click.option('-m', '--mountpoint', required=True, help=f'The mountpoint to check')
+@click.option('-m', '--mountpoint', required=True, multiple=True, help=f'The mountpoint to check')
 @click.option('-u', '--unit', required=False, default='auto', type=click.Choice(util.get_valid_units()), help=f'The unit to use for output display')
-@click.option('-l', '--label', required=True, help=f'A unique label to use')
 @click.option('-s', '--show-stats', is_flag=True, help=f'Gather disk statistics and display them in the tooltip')
 @click.option('-i', '--interval', type=int, default=5, help='The update interval (in seconds)')
 @click.option('-t', '--test', default=False, is_flag=True, help='Print the output and exit')
 @click.option('-d', '--debug', default=False, is_flag=True, help='Enable debug logging')
-def main(mountpoint, unit, label, show_stats, interval, test, debug):
-    global mountpoint_name, needs_fetch, needs_redraw
+def main(mountpoint, unit, show_stats, interval, test, debug):
+    global formats, needs_fetch, needs_redraw
 
-    logfile = cache_dir / f'waybar-filesystem-usage-{label}.log'
-    configure_logging(debug=debug, logfile=logfile)
-    mountpoint_name = mountpoint
+    configure_logging(debug=debug)
+    formats = list(range(len(mountpoint)))
 
     if test:
-        disk_info = get_disk_usage(mountpoint=mountpoint, show_stats=show_stats)
-        util.pprint(disk_info)
+        disk_info = get_disk_usage(mountpoints=mountpoint, show_stats=show_stats)
+        util.pprint(disk_info[0])
         print()
-        print(generate_tooltip(disk_info=disk_info, show_stats=show_stats))
+        print(generate_tooltip(disk_info=disk_info[0], show_stats=show_stats))
         return
 
     logging.info('[main] - entering')
